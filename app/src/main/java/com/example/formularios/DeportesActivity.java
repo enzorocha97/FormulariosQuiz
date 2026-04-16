b4f036ad99969c762859ac9f9a08bfa86a476043
enzorocha97 <enzo.rocha@uac.edu.co>
2026-04-15 22:03:55 -0500

    Actualizacion2

--- app/src/main/java/com/example/formularios/DeportesActivity.java
+++ app/src/main/java/com/example/formularios/DeportesActivity.java
@@ -1,6 +1,7 @@
 package com.example.formularios;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,12 @@ public class DeportesActivity extends AppCompatActivity {
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                SharedPreferences preferences = getSharedPreferences("MiPreferencia", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+
+                editor.clear();
+                editor.commit();
 
                 Intent intent = new Intent(DeportesActivity.this, MainActivity.class);
                 startActivity(intent);
